72731f139af58d2bc8b93cdb5532a6982277e9cd
jemoms <morej03@gmail.com>
2016-04-11 14:49:04 +0000

    decouverte_pb

---
+++ voyageur_de_commerce.rb
@@ -0,0 +1,47 @@
+villes = [
+ {
+   nom: "Paris",
+   latitude: 48.856578,
+   longitude: 2.351828
+ }, {
+   nom: "Nantes",
+   latitude: 47.21806,
+   longitude: -1.55278
+ }, {
+   nom: "Lyon",
+   latitude: 45.759723,
+   longitude: 4.842223
+ }, {
+   nom: "Bruxelles",
+   latitude: 50.846667,
+   longitude: 4.3525
+ }, {
+   nom: "Bordeaux",
+   latitude: 44.837912,
+   longitude: -0.579541
+ }, {
+   nom: "Toulouse",
+   latitude: 43.604482,
+   longitude: 1.443962
+ }, {
+   nom: "Montpellier",
+   latitude: 43.611944,
+   longitude: 3.877222
+ }, {
+   nom: "Marseille",
+   latitude: 43.296346,
+   longitude: 5.369889
+ }, {
+   nom: "Tours",
+   latitude: 47.393611,
+   longitude: 0.689167
+ }, {
+   nom: "Lille",
+   latitude: 50.637222,
+   longitude: 3.063333
+ }, {
+   nom: "Strasbourg",
+   latitude: 48.573392,
+   longitude: 7.752353
+ }
+]
